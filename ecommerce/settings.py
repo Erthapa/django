5202cae29fba0430a16b11cc6579a63ef70bff00
Erthapa <soniyathapa114@gmail.com>
2025-01-21 20:39:03 +0545

    initial commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'store/static'), 
+    
 ]
 MEDIA_URL = '/image/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/image')
